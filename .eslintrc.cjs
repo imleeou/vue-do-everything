ce78582a23a1dca375f6e667210d35bef2e4ebd5
l1ufe1 <l1ufei@163.com>
2022-09-21 17:54:54 +0800

    feat: 添加eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,7 +25,13 @@ module.exports = {
     // "off" or 0 - 关闭规则
     // "warn" or 1 - 将规则视为一个警告
     // "error" or 2 - 将规则视为一个错误
-    eqeqeq: 2, // 强制使用 === 和 !==
-    'vue/multi-word-component-names': 'off'
+     // 强制使用 === 和 !==
+    eqeqeq: 2,
+    "vue/eqeqeq": 2,
+    'vue/multi-word-component-names': 'off',
+    //一行结束后面不要有空格
+		"no-trailing-spaces": 1,
+		// 禁止使用var，用let和const代替
+		"no-var": 2,
   }
 }
