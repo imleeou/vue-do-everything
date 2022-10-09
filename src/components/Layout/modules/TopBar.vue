948626e1dd7453ff05f59939f5aa74ba183664bd
l1ufe1 <l1ufei@163.com>
2022-10-09 13:52:11 +0800

    feat: topBar 组件初始化

---
+++ src/components/Layout/modules/TopBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="top-bar">topbar</div>
+</template>
+
+<style scoped lang="less">
+.top-bar {
+  width: 100%;
+  height: 60px;
+  background-color: #333;
+  color: #fff;
+}
+</style>
