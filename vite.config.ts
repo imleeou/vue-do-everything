ed9e6b4d3ec08e5de6a411f2e3eafbb598bd754b
l1ufe1 <l1ufei@163.com>
2022-12-29 17:50:32 +0800

    style: 添加less全局变量

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        globalVars: {
+          hack: `true;@import '@/assets/css/variable.less'`
+        }
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
