e350830fcf01392b0ead4d6e68b75b692d33f7dd
liufei <l1ufei@163.com>
2023-07-01 17:13:40 +0800

    feat: 移除背景组件

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import TopBar from './modules/TopBar.vue'
-import BackGround from './modules/BackGround.vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 </script>
 
 <template>
   <template v-if="route.meta.showLayout">
-    <BackGround>
-      <TopBar></TopBar>
-      <slot></slot>
-    </BackGround>
+    <TopBar></TopBar>
+    <slot></slot>
   </template>
   <template v-else>
     <slot></slot>
