0e274939f2db443b37efb2377895558e31fef652
l1ufe1 <l1ufei@163.com>
2024-07-11 09:20:30 +0800

    chore: 修改端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -7,46 +7,52 @@ import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      less: {
-        globalVars: {
-          hack: `true;@import '@/assets/css/variable.less'`
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    server: {
+      port: +env.VITE_GLOB_PORT
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          globalVars: {
+            hack: `true;@import '@/assets/css/variable.less'`
+          }
         }
       }
-    }
-  },
-  plugins: [
-    vue(),
-    UnoCSS(),
-    AutoImport({
-      dts: true,
-      resolvers: [
-        ElementPlusResolver(),
-        // 自动导入图标组件
-        IconsResolver()
-      ]
-    }),
-    Components({
-      dts: true,
-      resolvers: [
-        ElementPlusResolver(),
-        // 自动注册图标组件
-        IconsResolver({
-          prefix: 'icon',
-          enabledCollections: ['ep']
-        })
-      ]
-    }),
-    Icons({
-      // compiler: 'vue3',
-      autoInstall: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': '/src'
+    },
+    plugins: [
+      vue(),
+      UnoCSS(),
+      AutoImport({
+        dts: true,
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver()
+        ]
+      }),
+      Components({
+        dts: true,
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            prefix: 'icon',
+            enabledCollections: ['ep']
+          })
+        ]
+      }),
+      Icons({
+        // compiler: 'vue3',
+        autoInstall: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
     }
   }
 })
